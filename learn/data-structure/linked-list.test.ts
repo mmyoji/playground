2d8d7f0bcd4ee9705ff428040a20cb5baa8f66af
mmyoji <mmyoji@gmail.com>
2022-07-02 20:31:55 +0900

    test(learn/data-structure): simplify with Deno.test

--- learn/data-structure/linked-list.test.ts
+++ learn/data-structure/linked-list.test.ts
@@ -1,122 +1,129 @@
 import { assertEquals, assertStrictEquals } from "testing/asserts.ts";
-import { describe, it } from "testing/bdd.ts";
 
 import { LinkedList } from "./linked-list.ts";
 
-describe("LinkedList", () => {
-  describe("new", () => {
-    it("has head=null and tail=null after initialization", () => {
-      const linkedList = new LinkedList();
+Deno.test("new LinkedList() sets head=null and tail=null", () => {
+  const list = new LinkedList<string>();
 
-      assertEquals(linkedList.head, null);
-      assertEquals(linkedList.tail, null);
-    });
-  });
-
-  describe("append", () => {
-    it("sets the same node for both head and tail with given value after initialization", () => {
-      const linkedList = new LinkedList();
-
-      linkedList.append("foo");
-
-      assertStrictEquals(linkedList.head, linkedList.tail);
-      assertEquals(linkedList.head!.value, "foo");
-      assertEquals(linkedList.tail!.value, "foo");
-      assertEquals(linkedList.head!.next, null);
-    });
-
-    it("sets a new tail with given value when the list is not empty", () => {
-      const linkedList = new LinkedList();
-
-      linkedList.append("foo");
-      linkedList.append("bar");
-
-      assertEquals(linkedList.head!.value, "foo");
-      assertStrictEquals(linkedList.head!.next, linkedList.tail);
-
-      assertEquals(linkedList.tail!.value, "bar");
-      assertEquals(linkedList.tail!.next, null);
-    });
-  });
-
-  describe("prepend", () => {
-    it("sets the same node for both head and tail with given value after initialization", () => {
-      const linkedList = new LinkedList();
-
-      linkedList.prepend("foo");
-
-      assertStrictEquals(linkedList.head, linkedList.tail);
-      assertEquals(linkedList.head!.value, "foo");
-      assertEquals(linkedList.tail!.value, "foo");
-      assertEquals(linkedList.head!.next, null);
-    });
-
-    it("sets a new head with given value when the list is not empty", () => {
-      const linkedList = new LinkedList();
-
-      linkedList.prepend("foo");
-      linkedList.prepend("bar");
-
-      assertEquals(linkedList.head!.value, "bar");
-      assertStrictEquals(linkedList.head!.next, linkedList.tail);
-
-      assertEquals(linkedList.tail!.value, "foo");
-      assertEquals(linkedList.tail!.next, null);
-    });
-  });
-
-  describe("find", () => {
-    it("returns null when the list is empty", () => {
-      const linkedList = new LinkedList<string>();
-
-      const result = linkedList.find("foo");
-      assertEquals(result, null);
-    });
-
-    it("returns null when the list doesn't have matched value", () => {
-      const linkedList = new LinkedList<string>();
-
-      linkedList.append("foo");
-      linkedList.append("bar");
-      linkedList.append("buz");
-
-      {
-        const result = linkedList.find("f");
-        assertEquals(result, null);
-      }
-
-      {
-        const result = linkedList.find("o");
-        assertEquals(result, null);
-      }
-
-      {
-        const result = linkedList.find("ba");
-        assertEquals(result, null);
-      }
-    });
-
-    it("returns matched node when the list has matched value", () => {
-      const linkedList = new LinkedList<string>();
-
-      linkedList.append("foo");
-      linkedList.append("bar");
-      linkedList.append("buz");
-
-      {
-        const result = linkedList.find("foo");
-        assertEquals(result!.value, "foo");
-      }
-
-      {
-        const result = linkedList.find("bar");
-        assertEquals(result!.value, "bar");
-      }
-
-      {
-        const result = linkedList.find("buz");
-        assertEquals(result!.value, "buz");
-      }
-    });
-  });
+  assertEquals(list.head, null);
+  assertEquals(list.tail, null);
 });
+
+Deno.test(
+  "LinkedList.append sets the same node for both head and tail with given value when the list has no head (tail)",
+  () => {
+    const list = new LinkedList<string>();
+
+    list.append("foo");
+
+    assertStrictEquals(list.head, list.tail);
+    assertEquals(list.head!.value, "foo");
+    assertEquals(list.tail!.value, "foo");
+    assertEquals(list.head!.next, null);
+  }
+);
+
+Deno.test(
+  "LinkedList.append sets a new tail with given value when the list is not empty",
+  () => {
+    const list = new LinkedList<string>();
+
+    list.append("foo");
+    list.append("bar");
+
+    assertEquals(list.head!.value, "foo");
+    assertStrictEquals(list.head!.next, list.tail);
+
+    assertEquals(list.tail!.value, "bar");
+    assertEquals(list.tail!.next, null);
+  }
+);
+
+Deno.test(
+  "LinkedList.prepend sets the same node for both head and tail with given value after initialization",
+  () => {
+    const list = new LinkedList<string>();
+
+    list.prepend("foo");
+
+    assertStrictEquals(list.head, list.tail);
+    assertEquals(list.head!.value, "foo");
+    assertEquals(list.tail!.value, "foo");
+    assertEquals(list.head!.next, null);
+  }
+);
+
+Deno.test(
+  "LinkedList.prepend sets a new head with given value when the list is not empty",
+  () => {
+    const list = new LinkedList<string>();
+
+    list.prepend("foo");
+    list.prepend("bar");
+
+    assertEquals(list.head!.value, "bar");
+    assertStrictEquals(list.head!.next, list.tail);
+
+    assertEquals(list.tail!.value, "foo");
+    assertEquals(list.tail!.next, null);
+  }
+);
+
+Deno.test("LinkedList.find returns null when the list is empty", () => {
+  const list = new LinkedList<string>();
+
+  const result = list.find("foo");
+  assertEquals(result, null);
+});
+
+Deno.test(
+  "LinkedList.find returns null when the list doesn't have matched value",
+  () => {
+    const list = new LinkedList<string>();
+
+    list.append("foo");
+    list.append("bar");
+    list.append("buz");
+
+    {
+      const result = list.find("f");
+      assertEquals(result, null);
+    }
+
+    {
+      const result = list.find("o");
+      assertEquals(result, null);
+    }
+
+    {
+      const result = list.find("ba");
+      assertEquals(result, null);
+    }
+  }
+);
+
+Deno.test(
+  "LinkedList.find returns matched node when the list has matched value",
+  () => {
+    const list = new LinkedList<string>();
+
+    list.append("foo");
+    list.append("bar");
+    list.append("buz");
+
+    {
+      const result = list.find("foo");
+      assertEquals(result!.value, "foo");
+    }
+
+    {
+      const result = list.find("bar");
+      assertEquals(result!.value, "bar");
+    }
+
+    {
+      const result = list.find("buz");
+      assertEquals(result!.value, "buz");
+    }
+  }
+);

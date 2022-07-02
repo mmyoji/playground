c9b5434b3744d6fff168698eb226444973d252d0
mmyoji <mmyoji@gmail.com>
2022-07-02 21:20:25 +0900

    refactor(learn/data-structure): tweak

--- learn/data-structure/linked-list.ts
+++ learn/data-structure/linked-list.ts
@@ -1,26 +1,28 @@
 // see: https://github.com/trekhleb/javascript-algorithms/tree/master/src/data-structures/linked-list
 
+type NodeType<T> = LinkedListNode<T> | null;
+
 class LinkedListNode<T> {
   value: T;
-  next: LinkedListNode<T> | null;
+  next: NodeType<T>;
 
-  constructor(value: T, next: LinkedListNode<T> | null = null) {
+  constructor(value: T, next: NodeType<T> = null) {
     this.value = value;
     this.next = next;
   }
 }
 
 export class LinkedList<T> {
-  head: LinkedListNode<T> | null;
-  tail: LinkedListNode<T> | null;
+  head: NodeType<T>;
+  tail: NodeType<T>;
 
   constructor() {
     this.head = null;
     this.tail = null;
   }
 
-  append(value: T): LinkedList<T> {
-    const newNode = new LinkedListNode<typeof value>(value);
+  append(value: T): this {
+    const newNode = new LinkedListNode<T>(value);
 
     if (!this.head) {
       this.head = newNode;
@@ -38,8 +40,8 @@ export class LinkedList<T> {
     return this;
   }
 
-  prepend(value: T): LinkedList<T> {
-    const newNode = new LinkedListNode<typeof value>(value, this.head);
+  prepend(value: T): this {
+    const newNode = new LinkedListNode<T>(value, this.head);
     this.head = newNode;
 
     if (!this.tail) {
@@ -49,12 +51,12 @@ export class LinkedList<T> {
     return this;
   }
 
-  find(value: T): LinkedListNode<T> | null {
+  find(value: T): NodeType<T> {
     if (!this.head) {
       return null;
     }
 
-    let current: LinkedListNode<T> | null = this.head;
+    let current: NodeType<T> = this.head;
 
     while (current) {
       if (current.value === value) {

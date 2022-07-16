825ff03c9ef68280178088106c33496419ca9b60
mmyoji <mmyoji@gmail.com>
2022-07-16 20:27:34 +0900

    style(apps/fresh): tweak error page styles

--- apps/fresh/routes/_404.tsx
+++ apps/fresh/routes/_404.tsx
@@ -1,7 +1,14 @@
 /** @jsx h */
 import { h } from "preact";
+import { tw } from "@twind";
 import { UnknownPageProps } from "$fresh/server.ts";
+import { Layout } from "../components/Layout.tsx";
 
 export default function NotFoundPage({ url }: UnknownPageProps) {
-  return <p>404 not found: {url.pathname}</p>;
+  return (
+    <Layout>
+      <h2 class={tw`font-bold text-3xl`}>404</h2>
+      <p>Not Found: {url.pathname}</p>
+    </Layout>
+  );
 }
--- apps/fresh/routes/_500.tsx
+++ apps/fresh/routes/_500.tsx
@@ -1,7 +1,14 @@
 /** @jsx h */
 import { h } from "preact";
+import { tw } from "@twind";
 import { ErrorPageProps } from "$fresh/server.ts";
+import { Layout } from "../components/Layout.tsx";
 
 export default function InternalServerErrorPage({ error }: ErrorPageProps) {
-  return <p>500 internal server error: {(error as Error).message}</p>;
+  return (
+    <Layout>
+      <h2 class={tw`font-bold text-3xl`}>500</h2>
+      <p>Internal Server Error: {(error as Error).message}</p>
+    </Layout>
+  );
 }

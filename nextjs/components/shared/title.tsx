90e7024d773d933dcc43dd1863d5c27eabc88bae
mmyoji <mmyoji@gmail.com>
2022-07-29 00:29:54 +0900

    chore(nextjs): convert Title css to CSS Modules

--- nextjs/components/shared/title.tsx
+++ nextjs/components/shared/title.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
 
+import styles from "./title.module.css";
+
 interface Props {
   children: ReactNode;
 }
 
 export function Title({ children }: Props) {
-  return <h1 className="font-bold text-xl">{children}</h1>;
+  return <h1 className={styles.title}>{children}</h1>;
 }
